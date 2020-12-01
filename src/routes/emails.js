376ffa01c4bd0d161c2b50df450c86b2809ab68e
I-FDesign <ifdesing.contacto@gmail.com>
2020-12-01 12:15:47 -0300

    Some little fixes

--- src/routes/emails.js
+++ src/routes/emails.js
@@ -24,7 +24,7 @@ app.post('/recover-password', (req, res) => {
         })
       }
 
-      const code = bcrypt.hashSync(Math.random().toString(36).substring(10), 10);
+      let code = bcrypt.hashSync(Math.random().toString(36).substring(10), 10);
 
       if(code.indexOf('/') >= 0) { // Removing "/"
         code = code.split('/');
