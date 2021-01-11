f4bcddf071760690fcfbb662534807d8c76eda7a
I-FDesign <ifdesing.contacto@gmail.com>
2021-01-11 18:27:48 -0300

    Port changed

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.post('/', (req, res) => {
     console.log(req.body.verification_code);
+
+    return res.status(200).json({
+        ok: true,
+        verification_code: req.body.verification_code
+    })
 })
 
 module.exports = app;
