079bbcb87314a11dc6a29bb4f63246bbd16a41b0
I-FDesign <ifdesing.contacto@gmail.com>
2021-04-01 20:36:40 -0300

    Credentials has changed

--- src/config/vars.js
+++ src/config/vars.js
@@ -1,7 +1,7 @@
 module.exports = {
     key: 'JSON-KEY-ASLN!31D!-OCULTUZ-2020-9090912',
-    openpayId: 'm2fxjpfrikgfxatrakxq',
-    openpayPrivateKey: 'sk_3c0aeca5b8c34495bc3bde4c1c485262',
+    openpayId: 'm1ukbh5ffbbfphgp3z4o',
+    openpayPrivateKey: 'sk_3f15ac6ccd824d97b0105a29e88f4285',
     merchantId: 'm2fxjpfrikgfxatrakxq',
     daysBeforeCancell: 1
 }
