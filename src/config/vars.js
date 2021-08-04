e5c14ca15a68742393c513bdb3bfcb4c112a3b4c
I-FDesign <ifdesing.contacto@gmail.com>
2021-08-04 17:55:45 -0300

    vars

--- src/config/vars.js
+++ src/config/vars.js
@@ -1,7 +1,7 @@
 module.exports = {
     key: 'JSON-KEY-ASLN!31D!-OCULTUZ-2020-9090912',
     openpayId: 'm1ukbh5ffbbfphgp3z4o',
-    mpAccessToken: 'TEST-2250573947337888-111301-a3d1541cf8e11603c5f12f1998b40338-222073650',
+    mpAccessToken: 'APP_USR-4794297338372023-071614-e51aeb7350c6cab8221432b781e81cb0-792221483',
     openpayPrivateKey: 'sk_3f15ac6ccd824d97b0105a29e88f4285',
     merchantId: 'm2fxjpfrikgfxatrakxq',
     daysBeforeCancell: 1
