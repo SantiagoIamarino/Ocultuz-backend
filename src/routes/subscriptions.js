41c81e21ef321c59e7887f3d4892849f0c30d10e
Santiago Iamarino <santiagoiamarino@gmail.com>
2021-09-07 19:19:15 -0300

    Fixing subs to test in prod

--- src/routes/subscriptions.js
+++ src/routes/subscriptions.js
@@ -147,7 +147,7 @@ app.post('/', (req, res) => {
                 const subscriptionRequest = {
                     "preapproval_plan_id":planId,
                     "card_token_id":body.cardToken,
-                    "payer_email": "test_user_9965551@testuser.com"
+                    "payer_email": userDB.email
                 };
             
                 axios.post('https://api.mercadopago.com/preapproval', subscriptionRequest, {
