9fae1382c56333f543d98c7dba8f53b8167e492f
Santiago Iamarino <santiago.iamarino@masglobalconsulting.com>
2022-10-26 13:33:02 -0300

    Mailjet fix

--- src/config/emails.js
+++ src/config/emails.js
@@ -1,60 +1,50 @@
 const EMAIL_SENDER = "soporte@ocultuz.com";
 
-const mailjet = require ('node-mailjet')
-  .connect(process.env.MAILJET_KEY, process.env.MAILJEY_SECRET)
+const mailjet = require("node-mailjet").connect(
+  process.env.KEY_MAILJET,
+  process.env.SECRET_MAILJET
+);
 
-const imageToBase64 = require('image-to-base64');
+const imageToBase64 = require("image-to-base64");
 
 function toBase64(path) {
-
   return new Promise((resolve, reject) => {
-
     imageToBase64(path)
-    .then(
-        (response) => {
-          resolve(response)
-        }
-    )
-    .catch(
-      (error) => {
-        console.log(error)
-        resolve(null)
+      .then((response) => {
+        resolve(response);
       })
-
-  })
-
-
+      .catch((error) => {
+        console.log(error);
+        resolve(null);
+      });
+  });
 }
 
 function sendEmail(data) {
-
-  const request = mailjet
-    .post("send", {'version': 'v3.1'})
-    .request({
-      "Messages":[
-        {
-          "From": {
-            "Email": EMAIL_SENDER,
-            "Name": "Ocultuz"
+  const request = mailjet.post("send", { version: "v3.1" }).request({
+    Messages: [
+      {
+        From: {
+          Email: EMAIL_SENDER,
+          Name: "Ocultuz",
+        },
+        To: [
+          {
+            Email: data.to,
+            Name: "Usuario Ocultuz",
           },
-          "To": [
-            {
-              "Email": data.to,
-              "Name": "Usuario Ocultuz"
-            }
-          ],
-          "Subject": data.subject,
-          "HTMLPart": data.html,
-          "Attachments": (data.attachments) ? data.attachments : []
-        }
-      ]
-    })
+        ],
+        Subject: data.subject,
+        HTMLPart: data.html,
+        Attachments: data.attachments ? data.attachments : [],
+      },
+    ],
+  });
 
-   return request;
-
+  return request;
 }
 
 module.exports = {
   toBase64,
-  sendEmail
-}
+  sendEmail,
+};
