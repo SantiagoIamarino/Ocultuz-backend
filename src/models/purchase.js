cf345d8e9b13ec539d461afba10480df796673a7
Santiago Iamarino <santiago.iamarino@masglobalconsulting.com>
2022-10-28 19:08:11 -0300

    Purchases fix

--- src/models/purchase.js
+++ src/models/purchase.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const purchaseSchema = new Schema({
-    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    girlId: { type: Schema.Types.ObjectId, ref: 'Girl', required: true },
-    contentType: { type: String, ref: 'Content' },
-    type: { type: String, required: true },
-    date: { type: Date, default: new Date() },
-    pending: { type: Boolean, default: false  },
-    hasBeenSent: { type: Boolean, default: false  },
-    paymentId: { type: String },
-    contentUrl: { type: String, default: null },
-    amount: { type: String, default: 0 }
-})
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  girlId: { type: Schema.Types.ObjectId, ref: "Girl", required: true },
+  description: { type: String, required: true },
+  contentType: { type: String },
+  type: { type: String, required: true },
+  date: { type: Date, default: new Date() },
+  pending: { type: Boolean, default: false },
+  hasBeenSent: { type: Boolean, default: false },
+  paymentId: { type: String },
+  contentUrl: { type: String, default: null },
+  amount: { type: String, default: 0 },
+});
 
-
-module.exports = mongoose.model('Purchase', purchaseSchema);
+module.exports = mongoose.model("Purchase", purchaseSchema);
